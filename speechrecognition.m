ec2eb379703ef1bfe1459f91815f0e40b9d6106b
Princee Dey <66840753+princeedey@users.noreply.github.com>
2020-11-26 23:30:47 +0530

    Add files via upload

---
+++ speechrecognition.m
@@ -0,0 +1,165 @@
+function speechrecognition(filename)
+%Speech Recognition Using Correlation Method
+%Write Following Command On Command Window
+%speechrecognition('test.wav')
+voice=audioread('test4.wav');
+x=voice;
+x=x';
+x=x(1,:);
+x=x';
+y1=audioread('bag.wav');
+y1=y1';
+y1=y1(1,:);
+y1=y1';
+z1=xcorr(x,y1);
+m1=max(z1);
+l1=length(z1);
+t1=-((l1-1)/2):1:((l1-1)/2);
+t1=t1';
+%subplot(3,2,1);
+plot(t1,z1);
+y2=audioread('biscuit.wav');
+y2=y2';
+y2=y2(1,:);
+y2=y2';
+z2=xcorr(x,y2);
+m2=max(z2);
+l2=length(z2);
+t2=-((l2-1)/2):1:((l2-1)/2);
+t2=t2';
+%subplot(3,2,2);
+figure
+plot(t2,z2);
+y3=audioread('bottle.wav');
+y3=y3';
+y3=y3(1,:);
+y3=y3';
+z3=xcorr(x,y3);
+m3=max(z3);
+l3=length(z3);
+t3=-((l3-1)/2):1:((l3-1)/2);
+t3=t3';
+%subplot(3,2,3);
+figure
+plot(t3,z3);
+y4=audioread('cat.wav');
+y4=y4';
+y4=y4(1,:);
+y4=y4';
+z4=xcorr(x,y4);
+m4=max(z4);
+l4=length(z4);
+t4=-((l4-1)/2):1:((l4-1)/2);
+t4=t4';
+%subplot(3,2,4);
+figure
+plot(t4,z4);
+y5=audioread('dog.wav');
+y5=y5';
+y5=y5(1,:);
+y5=y5';
+z5=xcorr(x,y5);
+m5=max(z5);
+l5=length(z5);
+t5=-((l5-1)/2):1:((l5-1)/2);
+t5=t5';
+%subplot(3,2,5);
+figure
+plot(t5,z5);
+y6=audioread('mouth.wav');
+y6=y6';
+y6=y6(1,:);
+y6=y6';
+z6=xcorr(x,y6);
+m6=max(z6);
+l6=length(z6);
+t6=-((l6-1)/2):1:((l6-1)/2);
+t6=t6';
+%subplot(3,2,6);
+figure
+plot(t6,z6);
+y7=audioread('one.wav');
+y7=y7';
+y7=y7(1,:);
+y7=y7';
+z7=xcorr(x,y7);
+m7=max(z7);
+l7=length(z7);
+t7=-((l7-1)/2):1:((l7-1)/2);
+t7=t7';
+%subplot(3,2,7);
+figure
+plot(t7,z7);
+y8=audioread('rat.wav');
+y8=y8';
+y8=y8(1,:);
+y8=y8';
+z8=xcorr(x,y8);
+m8=max(z8);
+l8=length(z8);
+t8=-((l8-1)/2):1:((l8-1)/2);
+t8=t8';
+%subplot(3,2,8);
+figure
+plot(t8,z8);
+y9=audioread('two.wav');
+y9=y9';
+y9=y9(1,:);
+y9=y9';
+z9=xcorr(x,y9);
+m9=max(z9);
+l9=length(z9);
+t9=-((l9-1)/2):1:((l9-1)/2);
+t9=t9';
+%subplot(3,2,9);
+figure
+plot(t9,z9);
+y10=audioread('water.wav');
+y10=y10';
+y10=y10(1,:);
+y10=y10';
+z10=xcorr(x,y10);
+m10=max(z10);
+l10=length(z10);
+t10=-((l10-1)/2):1:((l10-1)/2);
+t10=t10';
+%subplot(3,2,10);
+figure
+plot(t10,z10);
+a=[m1 m2 m3 m4 m5 m6 m7 m8 m9 m10];
+m=max(a);
+h=audioread('allow.wav');
+if m<=m1
+ soundsc(audioread('bag.wav'),50000)
+ soundsc(h,50000)
+elseif m<=m2
+ soundsc(audioread('biscuit.wav'),50000)
+ soundsc(h,50000)
+elseif m<=m3
+ soundsc(audioread('bottle.wav'),50000)
+ soundsc(h,50000)
+elseif m<=m4
+ soundsc(audioread('cat.wav'),50000)
+ soundsc(h,50000)
+elseif m<=m5
+ soundsc(audioread('dog.wav'),50000)
+ soundsc(h,50000)
+elseif m<=m6
+ soundsc(audioread('mouth.wav'),50000)
+ soundsc(h,50000)
+elseif m<=m7
+ soundsc(audioread('one.wav'),50000)
+ soundsc(h,50000)
+elseif m<=m8
+ soundsc(audioread('rat.wav'),50000)
+ soundsc(h,50000)
+elseif m<=m9
+ soundsc(audioread('two.wav'),50000)
+ soundsc(h,50000)
+elseif m<=m10
+ soundsc(audioread('water.wav'),50000)
+ soundsc(h,50000)
+else
+ soundsc(audioread('denied.wav'),50000)
+ soundsc(h,50000)
+end
